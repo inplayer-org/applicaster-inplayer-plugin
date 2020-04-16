b723b9b055db22c276f1f08314834644fdafe817
kononenkoAnton <a.kononenko@applicaster.com>
2020-04-16 12:38:32 -0400

    Add basic manifest for JWPlayer

--- plugins/jw-player/manifests/manifest.config.js
+++ plugins/jw-player/manifests/manifest.config.js
@@ -3,40 +3,32 @@
 const baseManifest = {
   api: {},
   dependency_repository_url: [],
-  dependency_name: "@applicaster/dev-demo-login",
+  dependency_name: "@applicaster/quick-brick-jw-player",
   author_name: "Applicaster",
   author_email: "zapp@applicaster.com",
-  name: "dev_demo_login",
-  description: "Quick Brick Developers Demo login plugin",
-  type: "general",
-  screen: true,
+  name: "JWPlayer QuickBrick",
+  description: "Quick Brick JW Player plugin",
+  type: "player",
   react_native: true,
-  identifier: "dev_demo_login",
+  identifier: "quick-brick-jw-player",
   ui_builder_support: true,
   whitelisted_account_ids: ["5e132c906663330008c0f8ab"],
-  min_zapp_sdk: "0.0.1",
   deprecated_since_zapp_sdk: "",
   unsupported_since_zapp_sdk: "",
-  preload: true,
   general: {
     fields: [
       {
-        type: "switch",
-        key: "is_flow_blocker",
-        tooltip_text: "Define if hook will block flow in case cancelation",
-        initial_value: false
+        type: "text",
+        key: "plist_JWPlayerKey",
+        tooltip_text: "JWPlayerKey Key",
+        default: "",
       },
-      {
-        type: "switch",
-        key: "allow_screen_plugin_presentation",
-        tooltip_text: "Define if",
-        initial_value: false
-      }
-    ]
+    ],
   },
+  npm_dependencies: ["react-native-jw-media-player@0.1.36"],
   custom_configuration_fields: [],
   targets: ["mobile"],
-  ui_frameworks: ["quickbrick"]
+  ui_frameworks: ["quickbrick"],
 };
 
 function createManifest({ version, platform }) {
@@ -44,10 +36,24 @@ function createManifest({ version, platform }) {
     ...baseManifest,
     platform,
     dependency_version: version,
-    manifest_version: version
+    manifest_version: version,
+    min_zapp_sdk: min_zapp_sdk[platform],
+    extra_dependencies: extra_dependencies[platform],
   };
 
   return manifest;
 }
 
+const min_zapp_sdk = {
+  tvos: "12.2.0-Dev",
+  ios: "20.2.0-Dev",
+};
+
+const extra_dependencies = {
+  ios: [
+    {
+      "JWPlayer-SDK": "'= 3.12.0'",
+    },
+  ],
+};
 module.exports = createManifest;
