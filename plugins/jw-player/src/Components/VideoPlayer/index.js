5ab80ca87714105aeeb4bd7621aec935c2ef66e4
kononenkoAnton <a.kononenko@applicaster.com>
2020-04-16 16:29:34 -0400

    Add jw player logic

--- plugins/jw-player/src/Components/VideoPlayer/index.js
+++ plugins/jw-player/src/Components/VideoPlayer/index.js
@@ -7,7 +7,11 @@ import {
   Dimensions,
   Platform,
 } from "react-native";
+
+import R from "ramda";
 import JWPlayer from "react-native-jw-media-player";
+import { parseJsonIfNeeded } from "@applicaster/zapp-react-native-utils/functionUtils";
+
 const { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
@@ -45,6 +49,51 @@ const styles = StyleSheet.create({
 });
 
 const VideoPlayer = (props) => {
+  const {
+    source: { uri, entry },
+    playableItem,
+    controls,
+  } = props;
+  console.log({ uri, entry, playableItem });
+
+  const playListItem = () => {
+    const { title = null, summary = null } = playableItem;
+    const url = R.isEmpty(uri) ? null : uri;
+
+    const image = R.compose(
+      R.prop("src"),
+      R.find(R.propEq("key", "image_base")),
+      R.prop("media_item"),
+      R.ifElse(Array.isArray, R.head, R.always(null)),
+      R.prop("media_group")
+    )(playableItem);
+
+    const mediaId = R.compose(
+      R.prop("player_id"),
+      parseJsonIfNeeded,
+      R.path(["extensions", "inPlayerData", "item", "content"])
+    )(playableItem);
+
+    // const { player_id, video_id } = jwPlayerData;
+
+    const retVal = {
+      title,
+      mediaId: R.isNil(mediaId) ? "-1" : mediaId,
+      image,
+      desc: summary,
+      time: 0,
+      file: url,
+      autostart: true,
+      controls,
+      repeat: false,
+      displayDescription: true,
+      displayTitle: true,
+    };
+    console.log({ url, image, controls, title, summary, mediaId, retVal });
+
+    return retVal;
+  };
+
   const onBeforePlay = () => {
     // eslint-disable-line
     // console.log('onBeforePlay was called');
@@ -56,24 +105,29 @@ const VideoPlayer = (props) => {
   };
 
   const onPlayerError = () => {
+    const { onError } = props;
+    // eslint-disable-line
+    onError(error);
     // eslint-disable-line
     console.log("onPlayerError was called with error: ", error);
   };
 
   const onSetupPlayerError = (error) => {
+    const { onError } = props;
     // eslint-disable-line
+    onError(error);
     console.log("onSetupPlayerError was called with error: ", error);
   };
 
-  const onBuffer = () => {
+  const onBuffer = (e) => {
     // eslint-disable-line
     // console.log('onBuffer was called');
   };
 
-  const onTime = ({ position, duration } = {
+  const onTime = ({ position, duration }) => {
     // eslint-disable-line
     // console.log('onTime was called with: ', position, duration);
-  });
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -85,20 +139,16 @@ const VideoPlayer = (props) => {
           >{`Your configuration of JWPlayer is wrong.\n\nDid you forget to add your JW key to your ${
             Platform.OS === "ios" ? "plist" : "manifest"
           }?\nDid you add a playlistItem with at least a file paramter?`}</Text>
-          {/* <JWPlayer
+          <JWPlayer
             style={styles.player}
-            playlistItem={{
-              mediaId: "1",
-              file: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8",
-              autostart: false,
-            }}
+            playlistItem={playListItem()}
             onBeforePlay={onBeforePlay}
             onPlay={onPlay}
             onSetupPlayerError={onSetupPlayerError}
             onPlayerError={onPlayerError}
             onBuffer={onBuffer}
             onTime={onTime}
-          /> */}
+          />
         </View>
       </View>
     </SafeAreaView>
