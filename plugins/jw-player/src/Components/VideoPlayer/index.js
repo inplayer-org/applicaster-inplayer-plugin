19590f9c0a7c17f3f8aff913a7d928a887141d71
kononenkoAnton <a.kononenko@applicaster.com>
2020-05-05 12:27:50 -0400

    Update

--- plugins/jw-player/src/Components/VideoPlayer/index.js
+++ plugins/jw-player/src/Components/VideoPlayer/index.js
@@ -4,7 +4,9 @@ import {
   releaseOrientationsForScreen,
   ORIENTATIONS,
 } from "@applicaster/zapp-react-native-utils/appUtils/orientationHelper";
-import { View, Dimensions, Platform } from "react-native";
+import { View, Dimensions, Platform, SafeAreaView } from "react-native";
+
+import { useDimensions } from "@applicaster/zapp-react-native-utils/reactHooks/layout";
 
 import R from "ramda";
 import JWPlayer from "react-native-jw-media-player-applicaster";
@@ -15,9 +17,11 @@ const { width, height } = Dimensions.get("window");
 // Player mediaId: "X8hnQSj3"
 // Entry  videoId: "3IAGTDeS"
 //content.jwplatform.com/players/3IAGTDeS-XeVTBbkG
-// https://content.jwplatform.com/videos/IfLGdOMF-dZH6UPjW.mp4
+// https://content.jwplatform.com/v""ideos/IfLGdOMF-dZH6UPjW.mp4
 const VideoPlayer = (props) => {
+  console.log("JWPlayer");
   const navigator = useNavigation();
+  const { height: screenHeight } = useDimensions("window", true);
 
   // const [isControlsVisible, setIsControlsVisible] = useState(true);
   useEffect(() => {
@@ -49,21 +53,21 @@ const VideoPlayer = (props) => {
       R.ifElse(Array.isArray, R.head, R.always(null)),
       R.prop("media_group")
     )(playableItem);
-
+    //uri,
     const retVal = {
       title,
       mediaId: advertismentMediaId(playableItem),
       image,
       desc: summary,
       time: 0,
-      file: uri,
+      file: "https://cdn.jwplayer.com/manifests/3IAGTDeS.m3u8",
       autostart: true,
       controls: customControls,
       repeat: false,
       displayDescription: true,
       displayTitle: true,
     };
-
+    console.log("Playlist Item", { retVal });
     return retVal;
   };
 
@@ -74,26 +78,36 @@ const VideoPlayer = (props) => {
   };
 
   const onPlay = (e) => {
-    props.onPlaybackRateChange({ playbackRate: 1 });
+    console.log("onPlay", { e });
+    // props.onPlaybackRateChange({ playbackRate: 1 });
   };
 
   const onPause = (e) => {
-    props.onPlaybackRateChange({ playbackRate: 0 });
+    console.log("onPause", { e });
+    // props.onPlaybackRateChange({ playbackRate: 0 });
   };
 
   const onPlayerError = (error) => {
     const { onError } = props;
+    console.log(
+      "onPlayerError was called with error: ",
+      error,
+      error.message,
+      error.nativeEvent
+    );
+
     props.onError(error);
-    console.log("onPlayerError was called with error: ", error);
   };
 
   const onSetupPlayerError = (error) => {
     const { onError } = props;
+    console.log("onSetupPlayerError was called with error: ", { error });
+
     props.onError(error);
-    console.log("onSetupPlayerError was called with error: ", error);
   };
 
   const onTime = (e) => {
+    // console.log("onTime", { e });
     const {
       nativeEvent: { position, duration },
     } = e;
@@ -101,15 +115,33 @@ const VideoPlayer = (props) => {
     if (position === 0) {
       props.onLoad({ duration });
     } else {
-      props.onProgress({ currentTime: position });
+      // props.onProgress({ currentTime: position });
     }
   };
 
-  console.log("Props", { props: props, width, height });
+  const onLayout = ({ nativeEvent }) => {
+    console.log({ nativeEvent });
+  };
+  console.log("VIDEO_PLAYER_LAYER", {
+    props: props,
+    width,
+    height,
+    ref: props.forwardedRef,
+    mee: this,
+    plugins: this.__plugin_configuration,
+  });
   const { onEnd, onFullscreenPlayerDidDismiss } = props;
   return (
-    <View style={{ flex: 1 }}>
+    <View
+      onLayout={onLayout}
+      style={{
+        // flex: 0, // CHeck if iOS needed it
+        height: screenHeight,
+        paddingBottom: Platform.OS === "android" ? 23 : 0,
+      }}
+    >
       <JWPlayer
+        // configuration={props.conn}
         style={{ flex: 1 }}
         playlistItem={playListItem()}
         onPlay={onPlay}
@@ -122,7 +154,7 @@ const VideoPlayer = (props) => {
         nativeFullScreen={false}
         fullScreenOnLandscape={false}
         landscapeOnFullScreen={false}
-        playerStyle={"test"}
+        // playerStyle={"test"}
       />
     </View>
   );
