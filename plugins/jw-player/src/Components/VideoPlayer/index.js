9225f80e3f4aaf8c129762bdf44f01270b4c8f07
kononenkoAnton <a.kononenko@applicaster.com>
2020-05-05 13:55:18 -0400

    Add refactor of jwplayer

--- plugins/jw-player/src/Components/VideoPlayer/index.js
+++ plugins/jw-player/src/Components/VideoPlayer/index.js
@@ -1,23 +1,18 @@
 import React, { useEffect } from "react";
+import R from "ramda";
+import { View, Platform } from "react-native";
 import {
   allowedOrientationsForScreen,
   releaseOrientationsForScreen,
   ORIENTATIONS,
 } from "@applicaster/zapp-react-native-utils/appUtils/orientationHelper";
-import { View, Dimensions, Platform, SafeAreaView } from "react-native";
-
 import { useDimensions } from "@applicaster/zapp-react-native-utils/reactHooks/layout";
-
-import R from "ramda";
-import JWPlayer from "react-native-jw-media-player-applicaster";
 import { useNavigation } from "@applicaster/zapp-react-native-utils/reactHooks/navigation";
+import JWPlayer from "react-native-jw-media-player-applicaster";
 
-const { width, height } = Dimensions.get("window");
+// export default class VideoPlayer extends Component {
 
-// Player mediaId: "X8hnQSj3"
-// Entry  videoId: "3IAGTDeS"
-//content.jwplatform.com/players/3IAGTDeS-XeVTBbkG
-// https://content.jwplatform.com/v""ideos/IfLGdOMF-dZH6UPjW.mp4
+// }
 const VideoPlayer = (props) => {
   console.log("JWPlayer");
   const navigator = useNavigation();
@@ -42,34 +37,8 @@ const VideoPlayer = (props) => {
     playableItem,
     controls: customControls,
   } = props;
-  console.log({ width, height, uri, entry, playableItem });
-  const playListItem = () => {
-    const { title = null, summary = null } = playableItem;
 
-    const image = R.compose(
-      R.prop("src"),
-      R.find(R.propEq("key", "image_base")),
-      R.prop("media_item"),
-      R.ifElse(Array.isArray, R.head, R.always(null)),
-      R.prop("media_group")
-    )(playableItem);
-    //uri,
-    const retVal = {
-      title,
-      mediaId: advertismentMediaId(playableItem),
-      image,
-      desc: summary,
-      time: 0,
-      file: "https://cdn.jwplayer.com/manifests/3IAGTDeS.m3u8",
-      autostart: true,
-      controls: customControls,
-      repeat: false,
-      displayDescription: true,
-      displayTitle: true,
-    };
-    console.log("Playlist Item", { retVal });
-    return retVal;
-  };
+  console.log({ width, height, uri, entry, playableItem });
 
   const advertismentMediaId = (playableItem) => {
     console.log("advertismentMediaId", { playableItem });
@@ -130,20 +99,19 @@ const VideoPlayer = (props) => {
     mee: this,
     plugins: this.__plugin_configuration,
   });
-  const { onEnd, onFullscreenPlayerDidDismiss } = props;
+  const { onEnd, onFullscreenPlayerDidDismiss, pluginConfiguration } = props;
   return (
     <View
       onLayout={onLayout}
       style={{
-        // flex: 0, // CHeck if iOS needed it
         height: screenHeight,
         paddingBottom: Platform.OS === "android" ? 23 : 0,
       }}
     >
       <JWPlayer
-        // configuration={props.conn}
+        licenceKey={jwLicenceKey(pluginConfiguration)}
         style={{ flex: 1 }}
-        playlistItem={playListItem()}
+        playlistItem={playListItem(props)}
         onPlay={onPlay}
         onPause={onPause}
         onSetupPlayerError={onSetupPlayerError}
@@ -161,3 +129,45 @@ const VideoPlayer = (props) => {
 };
 
 export default VideoPlayer;
+
+const jwLicenceKey = (pluginConfiguration) => {
+  return pluginConfiguration?.jw_player_android_key;
+};
+
+const playListItem = (props) => {
+  const {
+    source: { uri },
+    playableItem,
+    controls: customControls,
+  } = props;
+
+  const { title = null, summary = null } = playableItem;
+
+  const image = imageFromPlayableItem(playableItem);
+  //"https://cdn.jwplayer.com/manifests/3IAGTDeS.m3u8",
+  const retVal = {
+    title,
+    mediaId: advertismentMediaId(playableItem),
+    image,
+    desc: summary,
+    time: 0,
+    file: uri,
+    autostart: true,
+    controls: customControls,
+    repeat: false,
+    displayDescription: true,
+    displayTitle: true,
+  };
+  console.log("Playlist Item", { retVal });
+  return retVal;
+};
+
+const imageFromPlayableItem = (playableItem) => {
+  return R.compose(
+    R.prop("src"),
+    R.find(R.propEq("key", "image_base")),
+    R.prop("media_item"),
+    R.ifElse(Array.isArray, R.head, R.always(null)),
+    R.prop("media_group")
+  )(playableItem);
+};
