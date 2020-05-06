f5d03b3c8bfaca5343f19659d6338220faedd5a1
kononenkoAnton <a.kononenko@applicaster.com>
2020-05-06 17:42:06 -0400

    add jw player android support

--- plugins/jw-player/src/Components/VideoPlayer/index.ios.js
+++ plugins/jw-player/src/Components/VideoPlayer/index.ios.js
@@ -1,131 +1,58 @@
-import React, { useEffect } from "react";
-import {
-  allowedOrientationsForScreen,
-  releaseOrientationsForScreen,
-  ORIENTATIONS,
-} from "@applicaster/zapp-react-native-utils/appUtils/orientationHelper";
-import { View, Dimensions, Platform } from "react-native";
-
-import R from "ramda";
+import React from "react";
+import { View } from "react-native";
+import { playListItem } from "../../Utils";
 import JWPlayer from "react-native-jw-media-player-applicaster";
-import { useNavigation } from "@applicaster/zapp-react-native-utils/reactHooks/navigation";
-
-const { width, height } = Dimensions.get("window");
-
-// Player mediaId: "X8hnQSj3"
-// Entry  videoId: "3IAGTDeS"
-//content.jwplatform.com/players/3IAGTDeS-XeVTBbkG
-// https://content.jwplatform.com/videos/IfLGdOMF-dZH6UPjW.mp4
-const VideoPlayer = (props) => {
-  const navigator = useNavigation();
-
-  // const [isControlsVisible, setIsControlsVisible] = useState(true);
-  useEffect(() => {
-    console.log("Mount");
-    allowedOrientationsForScreen(ORIENTATIONS.landscapeSensor);
-  }, []);
-
-  useEffect(
-    () => () => {
-      console.log("UN-Mount");
-      releaseOrientationsForScreen();
-    },
-    []
-  );
-
-  const {
-    source: { uri, entry },
-    playableItem,
-    controls: customControls,
-  } = props;
-  console.log({ width, height, uri, entry, playableItem });
-  const playListItem = () => {
-    const { title = null, summary = null } = playableItem;
-
-    const image = R.compose(
-      R.prop("src"),
-      R.find(R.propEq("key", "image_base")),
-      R.prop("media_item"),
-      R.ifElse(Array.isArray, R.head, R.always(null)),
-      R.prop("media_group")
-    )(playableItem);
-
-    const retVal = {
-      title,
-      mediaId: advertismentMediaId(playableItem),
-      image,
-      desc: summary,
-      time: 0,
-      file: uri,
-      autostart: true,
-      controls: customControls,
-      repeat: false,
-      displayDescription: true,
-      displayTitle: true,
-    };
-
-    return retVal;
+export default class VideoPlayer extends React.Component {
+  onPlay = () => {
+    const { onPlaybackRateChange } = this.props;
+    onPlaybackRateChange && onPlaybackRateChange({ playbackRate: 1 });
   };
 
-  const advertismentMediaId = (playableItem) => {
-    console.log("advertismentMediaId", { playableItem });
-    const { id: entryId } = playableItem;
-    return entryId || "-1";
+  onPause = () => {
+    const { onPlaybackRateChange } = this.props;
+    onPlaybackRateChange && onPlaybackRateChange({ playbackRate: 0 });
   };
 
-  const onPlay = (e) => {
-    props.onPlaybackRateChange({ playbackRate: 1 });
+  onError = ({ nativeEvent }) => {
+    const { onError } = this.props;
+    onError &&
+      onError({ error: { localizedDescription: nativeEvent?.message } });
   };
 
-  const onPause = (e) => {
-    props.onPlaybackRateChange({ playbackRate: 0 });
-  };
+  onTime = ({ nativeEvent }) => {
+    const { onLoad, onProgress } = this.props;
+    const { position, duration } = nativeEvent;
 
-  const onPlayerError = (error) => {
-    const { onError } = props;
-    props.onError(error);
-    console.log("onPlayerError was called with error: ", error);
-  };
-
-  const onSetupPlayerError = (error) => {
-    const { onError } = props;
-    props.onError(error);
-    console.log("onSetupPlayerError was called with error: ", error);
-  };
-
-  const onTime = (e) => {
-    const {
-      nativeEvent: { position, duration },
-    } = e;
-    // eslint-disable-line
     if (position === 0) {
-      props.onLoad({ duration });
+      onLoad({ duration });
     } else {
-      props.onProgress({ currentTime: position });
+      onProgress({ currentTime: position });
     }
   };
 
-  console.log("Props", { props: props, width, height });
-  const { onEnd, onFullscreenPlayerDidDismiss } = props;
-  return (
-    <View style={{ flex: 1 }}>
-      <JWPlayer
-        style={{ flex: 1 }}
-        playlistItem={playListItem()}
-        onPlay={onPlay}
-        onPause={onPause}
-        onSetupPlayerError={onSetupPlayerError}
-        onPlayerError={onPlayerError}
-        onTime={onTime}
-        onComplete={onEnd}
-        onClose={onFullscreenPlayerDidDismiss}
-        nativeFullScreen={false}
-        fullScreenOnLandscape={false}
-        landscapeOnFullScreen={false}
-        playerStyle={"test"}
-      />
-    </View>
-  );
-};
-
-export default VideoPlayer;
+  render() {
+    const { onEnd, onFullscreenPlayerDidDismiss } = this.props;
+    return (
+      <View style={{ flex: 1 }}>
+        <JWPlayer
+          ref={this.assignPlayerRef}
+          style={{ flex: 1 }}
+          playlistItem={playListItem(this.props)}
+          onPlay={this.onPlay}
+          onPause={this.onPause}
+          onPlayerReady={this.onPlayerReady}
+          onSetupPlayerError={this.onError}
+          onPlayerError={this.onError}
+          onTime={this.onTime}
+          onBuffer={this.onBuffer}
+          onComplete={onEnd}
+          onClose={onFullscreenPlayerDidDismiss}
+          nativeFullScreen={false}
+          fullScreenOnLandscape={false}
+          landscapeOnFullScreen={false}
+          // playerStyle={"test"}
+        />
+      </View>
+    );
+  }
+}
--- plugins/jw-player/src/Components/VideoPlayer/index.js
+++ plugins/jw-player/src/Components/VideoPlayer/index.js
@@ -1,173 +1,72 @@
-import React, { useEffect } from "react";
-import R from "ramda";
-import { View, Platform } from "react-native";
-import {
-  allowedOrientationsForScreen,
-  releaseOrientationsForScreen,
-  ORIENTATIONS,
-} from "@applicaster/zapp-react-native-utils/appUtils/orientationHelper";
-import { useDimensions } from "@applicaster/zapp-react-native-utils/reactHooks/layout";
-import { useNavigation } from "@applicaster/zapp-react-native-utils/reactHooks/navigation";
+import * as React from "react";
+import { View, Platform, Dimensions } from "react-native";
 import JWPlayer from "react-native-jw-media-player-applicaster";
-
-// export default class VideoPlayer extends Component {
-
-// }
-const VideoPlayer = (props) => {
-  console.log("JWPlayer");
-  const navigator = useNavigation();
-  const { height: screenHeight } = useDimensions("window", true);
-
-  // const [isControlsVisible, setIsControlsVisible] = useState(true);
-  useEffect(() => {
-    console.log("Mount");
-    allowedOrientationsForScreen(ORIENTATIONS.landscapeSensor);
-  }, []);
-
-  useEffect(
-    () => () => {
-      console.log("UN-Mount");
-      releaseOrientationsForScreen();
-    },
-    []
-  );
-
-  const {
-    source: { uri, entry },
-    playableItem,
-    controls: customControls,
-  } = props;
-
-  console.log({ width, height, uri, entry, playableItem });
-
-  const advertismentMediaId = (playableItem) => {
-    console.log("advertismentMediaId", { playableItem });
-    const { id: entryId } = playableItem;
-    return entryId || "-1";
+import { jwLicenceKey, playListItem } from "../../Utils";
+export default class VideoPlayer extends React.Component {
+  onPlay = () => {
+    const { onPlay } = this.props;
+    onPlay({ type: "play" });
   };
 
-  const onPlay = (e) => {
-    console.log("onPlay", { e });
-    // props.onPlaybackRateChange({ playbackRate: 1 });
+  onPause = () => {
+    const { onPause } = this.props;
+    onPause({ type: "pause" });
   };
 
-  const onPause = (e) => {
-    console.log("onPause", { e });
-    // props.onPlaybackRateChange({ playbackRate: 0 });
+  onPlayerReady = () => {
+    const { onLoadedData } = this.props;
+    onLoadedData && onLoadedData({ type: "loadeddata" });
   };
 
-  const onPlayerError = (error) => {
-    const { onError } = props;
-    console.log(
-      "onPlayerError was called with error: ",
-      error,
-      error.message,
-      error.nativeEvent
+  onError = ({ nativeEvent }) => {
+    const { onError } = this.props;
+    onError({ type: "error", target: { error: nativeEvent?.message } });
+  };
+
+  onBuffer = () => {
+    const { updatePlayerState } = this.props;
+    updatePlayerState({ type: "loadstart" });
+  };
+
+  onTime = ({ nativeEvent }) => {
+    const { onProgress } = this.props;
+    const { position, duration } = nativeEvent;
+
+    onProgress && onProgress({ type: "progress" }, { position, duration });
+  };
+  assignPlayerRef(ref) {
+    player = ref;
+  }
+
+  render() {
+    const { width, height } = Dimensions.get("window");
+    const { pluginConfiguration, onEnded, autoplay } = this.props;
+    return (
+      <View
+        style={{
+          height: Math.min(width, height),
+          paddingBottom: Platform.OS === "android" ? 23 : 0,
+        }}
+      >
+        <JWPlayer
+          ref={this.assignPlayerRef}
+          licenceKey={jwLicenceKey(pluginConfiguration)}
+          style={{ flex: 1 }}
+          playlistItem={playListItem(this.props)}
+          onPlay={this.onPlay}
+          onPause={this.onPause}
+          onPlayerReady={this.onPlayerReady}
+          onSetupPlayerError={this.onError}
+          onPlayerError={this.onError}
+          onBuffer={this.onBuffer}
+          onTime={this.onTime}
+          onComplete={onEnded}
+          nativeFullScreen={false}
+          fullScreenOnLandscape={false}
+          landscapeOnFullScreen={false}
+          autostart={autoplay}
+        />
+      </View>
     );
-
-    props.onError(error);
-  };
-
-  const onSetupPlayerError = (error) => {
-    const { onError } = props;
-    console.log("onSetupPlayerError was called with error: ", { error });
-
-    props.onError(error);
-  };
-
-  const onTime = (e) => {
-    // console.log("onTime", { e });
-    const {
-      nativeEvent: { position, duration },
-    } = e;
-    // eslint-disable-line
-    if (position === 0) {
-      props.onLoad({ duration });
-    } else {
-      // props.onProgress({ currentTime: position });
-    }
-  };
-
-  const onLayout = ({ nativeEvent }) => {
-    console.log({ nativeEvent });
-  };
-  console.log("VIDEO_PLAYER_LAYER", {
-    props: props,
-    width,
-    height,
-    ref: props.forwardedRef,
-    mee: this,
-    plugins: this.__plugin_configuration,
-  });
-  const { onEnd, onFullscreenPlayerDidDismiss, pluginConfiguration } = props;
-  return (
-    <View
-      onLayout={onLayout}
-      style={{
-        height: screenHeight,
-        paddingBottom: Platform.OS === "android" ? 23 : 0,
-      }}
-    >
-      <JWPlayer
-        licenceKey={jwLicenceKey(pluginConfiguration)}
-        style={{ flex: 1 }}
-        playlistItem={playListItem(props)}
-        onPlay={onPlay}
-        onPause={onPause}
-        onSetupPlayerError={onSetupPlayerError}
-        onPlayerError={onPlayerError}
-        onTime={onTime}
-        onComplete={onEnd}
-        onClose={onFullscreenPlayerDidDismiss}
-        nativeFullScreen={false}
-        fullScreenOnLandscape={false}
-        landscapeOnFullScreen={false}
-        // playerStyle={"test"}
-      />
-    </View>
-  );
-};
-
-export default VideoPlayer;
-
-const jwLicenceKey = (pluginConfiguration) => {
-  return pluginConfiguration?.jw_player_android_key;
-};
-
-const playListItem = (props) => {
-  const {
-    source: { uri },
-    playableItem,
-    controls: customControls,
-  } = props;
-
-  const { title = null, summary = null } = playableItem;
-
-  const image = imageFromPlayableItem(playableItem);
-  //"https://cdn.jwplayer.com/manifests/3IAGTDeS.m3u8",
-  const retVal = {
-    title,
-    mediaId: advertismentMediaId(playableItem),
-    image,
-    desc: summary,
-    time: 0,
-    file: uri,
-    autostart: true,
-    controls: customControls,
-    repeat: false,
-    displayDescription: true,
-    displayTitle: true,
-  };
-  console.log("Playlist Item", { retVal });
-  return retVal;
-};
-
-const imageFromPlayableItem = (playableItem) => {
-  return R.compose(
-    R.prop("src"),
-    R.find(R.propEq("key", "image_base")),
-    R.prop("media_item"),
-    R.ifElse(Array.isArray, R.head, R.always(null)),
-    R.prop("media_group")
-  )(playableItem);
-};
+  }
+}
