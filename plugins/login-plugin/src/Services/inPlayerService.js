1d5f20d815538b9108c09ef27b7b090887912be4
kononenkoAnton <a.kononenko@applicaster.com>
2020-05-26 16:27:57 -0400

    Fix Api to validate

--- plugins/login-plugin/src/Services/inPlayerService.js
+++ plugins/login-plugin/src/Services/inPlayerService.js
@@ -176,7 +176,7 @@ export async function getLastEmailUsed() {
 
 function externalPurchaseValidationURL() {
   // URL Example: https://staging-v2.inplayer.com/v2/external-payments/apple/validate
-  const platform = Platform.OS === "ios" ? "apple" : "android";
+  const platform = Platform.OS === "ios" ? "apple" : "google-play";
   return `${InPlayer.config.BASE_URL}/v2/external-payments/${platform}/validate`;
 }
 
