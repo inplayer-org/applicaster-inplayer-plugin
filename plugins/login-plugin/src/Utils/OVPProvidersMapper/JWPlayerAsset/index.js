bd9a1b8640823d6d0dd3868e7097677cc994e419
Egor Brel <brel@scand.com>
2020-06-23 12:03:09 +0300

    Added check for mobile_url in response.

--- plugins/login-plugin/src/Utils/OVPProvidersMapper/JWPlayerAsset/index.js
+++ plugins/login-plugin/src/Utils/OVPProvidersMapper/JWPlayerAsset/index.js
@@ -1,8 +1,14 @@
+import { Platform } from 'react-native';
 export const getSrcForJWPlayer = ({ inPlayerItemAccess, inPlayerContent }) => {
   return inPlayerContent ? JWPlayerContent(inPlayerContent) : null;
 };
 const JWPlayerContent = (inPlayerContent) => {
   console.log("JWPlayerContent", { inPlayerContent });
+  const { mobile_url = null } = inPlayerContent;
+  if (mobile_url && (Platform.OS === 'ios' || Platform.OS === 'android')) {
+    return mobile_url;
+  }
+
   const { video_id = null, stream_url = null } = inPlayerContent;
   return stream_url || JWPlayerContentFromMediaID(video_id);
 };
