ab633832711fbaf8f34497fb24b55d383471577f
Egor Brel <brel@scand.com>
2020-06-23 13:04:34 +0300

    Added web url check

--- plugins/login-plugin/src/Utils/OVPProvidersMapper/JWPlayerAsset/index.js
+++ plugins/login-plugin/src/Utils/OVPProvidersMapper/JWPlayerAsset/index.js
@@ -1,12 +1,19 @@
 import { Platform } from 'react-native';
+
 export const getSrcForJWPlayer = ({ inPlayerItemAccess, inPlayerContent }) => {
   return inPlayerContent ? JWPlayerContent(inPlayerContent) : null;
 };
 const JWPlayerContent = (inPlayerContent) => {
   console.log("JWPlayerContent", { inPlayerContent });
-  const { mobile_url = null } = inPlayerContent;
-  if (mobile_url && (Platform.OS === 'ios' || Platform.OS === 'android')) {
+
+  const mobilePlatform = Platform.OS === 'ios' || Platform.OS === 'android';
+  const webPlatform = Platform.OS === 'web';
+  const { mobile_url = null, web_url = null} = inPlayerContent;
+  if (mobile_url && mobilePlatform) {
     return mobile_url;
+  }
+  if (web_url && webPlatform) {
+    return web_url;
   }
 
   const { video_id = null, stream_url = null } = inPlayerContent;
