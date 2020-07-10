171d76e33cc8cc5e2c408b6fba9a1dcea89c3445
oduk <oduk@scand.com>
2020-07-10 13:40:41 +0300

    fix(manifest): fixed labels and default values in manifest.

--- plugins/login-plugin/manifests/manifest.config.js
+++ plugins/login-plugin/manifests/manifest.config.js
@@ -342,13 +342,14 @@ const baseManifest = {
             type: "uploader",
             label: "Close Button",
             label_tooltip: "Icon for close button. Dimensions 45 x 45.",
-            placeholder: "Default close Icon image",
+            placeholder: "W 45px x H 45px",
           },
           {
             key: "client_logo",
             type: "uploader",
             label: "Client Logo",
             label_tooltip: "Client logo image. Dimension 200 x44.",
+            placeholder: "W 200px x H 44px",
           },
           {
             key: "payment_screen_title_text",
@@ -361,7 +362,6 @@ const baseManifest = {
           {
             key: "payment_screen_title_font_ios",
             type: "ios_font_selector",
-            label: "Payment Screen Title Font",
             label_tooltip: "Font for Main Title for ios.",
             initial_value: "Roboto-Bold",
           },
@@ -394,7 +394,6 @@ const baseManifest = {
           {
             key: "restore_purchases_text_font_ios",
             type: "ios_font_selector",
-            label: "Restore Purchases Description Text Font",
             label_tooltip:
               "Font for Restore Purchases Description Text for ios.",
             initial_value: "Roboto-Regular",
@@ -429,7 +428,6 @@ const baseManifest = {
           {
             key: "restore_purchases_link_font_ios",
             type: "ios_font_selector",
-            label: "Restore Purchases Link Text Font",
             label_tooltip: "Font for Restore Purchases Link Text for ios.",
             initial_value: "Roboto-Bold",
           },
@@ -468,7 +466,6 @@ const baseManifest = {
           {
             key: "payment_option_title_font_ios",
             type: "ios_font_selector",
-            label: "Payment Option Title Font",
             label_tooltip: "Font for the Payment Option Title for ios.",
             initial_value: "Roboto-Bold",
           },
@@ -488,12 +485,11 @@ const baseManifest = {
             key: "payment_option_title_fontcolor",
             type: "color_picker_rgba",
             label_tooltip: "Font Color for Payment Option Title.",
-            initial_value: "rgba(255, 255, 255, 1)",
+            initial_value: "rgba(0,0,0,1)",
           },
           {
             key: "payment_option_description_font_ios",
             type: "ios_font_selector",
-            label: "Payment Option Description Font",
             label_tooltip: "Font for the Payment Option Description for ios.",
             initial_value: "Roboto-Regular",
           },
@@ -514,7 +510,7 @@ const baseManifest = {
             key: "payment_option_description_fontcolor",
             type: "color_picker_rgba",
             label_tooltip: "Font Color for Payment Option Description.",
-            initial_value: "rgba(255, 255, 255, 1)",
+            initial_value: "rgba(66,74,87,1)",
           },
           {
             key: "payment_option_button_background",
@@ -535,7 +531,6 @@ const baseManifest = {
           {
             key: "payment_option_button_font_ios",
             type: "ios_font_selector",
-            label: "Payment Option Action Button Text Font",
             label_tooltip:
               "Font for the Payment Option Action Button Text for ios.",
             initial_value: "Roboto-Bold",
@@ -571,7 +566,6 @@ const baseManifest = {
           {
             key: "terms_of_use_instructions_font_ios",
             type: "ios_font_selector",
-            label: "Payment Terms of Use instructions Text Font",
             label_tooltip:
               "Font for the Terms of Use Instructions Text for ios.",
             initial_value: "Roboto-Regular",
@@ -596,6 +590,13 @@ const baseManifest = {
             initial_value: "rgba(255, 255, 255, 1)",
           },
           {
+            key: "terms_of_use_link",
+            type: "text_input",
+            label: "Payment Terms of use Link",
+            label_tooltip: "Link for the Terms of Use.",
+            initial_value: "http://google.com",
+          },
+          {
             key: "terms_of_use_link_text",
             type: "text_input",
             label: "Payment Terms of use Link Text",
@@ -605,7 +606,6 @@ const baseManifest = {
           {
             key: "terms_of_use_link_font_ios",
             type: "ios_font_selector",
-            label: "Payment Terms of Use Link Text Font",
             label_tooltip: "Font for the Terms of Use Link Text for ios.",
             initial_value: "Roboto-Regular",
           },
