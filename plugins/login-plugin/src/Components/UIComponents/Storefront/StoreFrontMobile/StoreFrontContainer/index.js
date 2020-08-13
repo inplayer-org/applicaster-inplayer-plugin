30872fb9b8ecc174f63633807ff9d55081e6d567
kononenkoAnton <a.kononenko@applicaster.com>
2020-08-13 13:29:48 -0400

    Difrenatiate assets

---
+++ plugins/login-plugin/src/Components/UIComponents/Storefront/StoreFrontMobile/StoreFrontContainer/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import PaymentOptionView from "../../PaymentOptionView";
+import { container } from "../../../Styles";
+import { mapKeyToStyle, withEndSpace } from "../../../../Utils/Customization";
+
+const storefrontStyleKeys = [
+  "payment_screen_title",
+  "restore_purchases_text",
+  "restore_purchases_link",
+];
+
+export default function StoreFrontContainer(props) {
+  const {
+    screenStyles,
+    dataSource,
+    onPressPaymentOption,
+    onPressRestore,
+  } = props;
+
+  const {
+    payment_screen_title_text: paymentTitle,
+    restore_purchases_text: restoreText,
+    restore_purchases_link: restoreLink,
+  } = screenStyles;
+
+  const [
+    paymentTitleStyle,
+    restoreTextStyle,
+    restoreLinkStyle,
+  ] = storefrontStyleKeys.map((key) => mapKeyToStyle(key, screenStyles));
+
+  return (
+    <View style={[styles.container, { paddingHorizontal: 25 }]}>
+      <Text style={paymentTitleStyle} numberOfLines={1} ellipsizeMode="tail">
+        {paymentTitle}
+      </Text>
+      <View style={styles.restoreContainer}>
+        <Text
+          style={[restoreTextStyle, { textAlign: "center" }]}
+          numberOfLines={2}
+          ellipsizeMode="tail"
+        >
+          {withEndSpace(restoreText)}
+          <Text style={restoreLinkStyle} onPress={onPressRestore}>
+            {restoreLink}
+          </Text>
+        </Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <ScrollView>
+          {dataSource.map((item, index) => (
+            <PaymentOptionView
+              screenStyles={screenStyles}
+              paymentOptionItem={item}
+              key={item.productIdentifier}
+              onPress={() => onPressPaymentOption(index)}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container,
+  restoreContainer: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+});
--- plugins/login-plugin/src/Utils/index.js
+++ plugins/login-plugin/src/Utils/index.js
@@ -1,4 +1,4 @@
-export const DEFAULT_NETWORK_TIMEOUT = 5000;
+export const DEFAULT_NETWORK_TIMEOUT = 15000;
 
 export function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
