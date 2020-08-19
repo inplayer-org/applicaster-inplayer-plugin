fd136c4dfd0e1712ae3c875d2acd3341f5107a0b
Pawel Buderaski <pbuderaski@gmail.com>
2020-08-19 14:21:42 +0100

    fix: ClientLogo source

--- plugins/login-plugin/src/Components/UIComponents/ClientLogo/index.js
+++ plugins/login-plugin/src/Components/UIComponents/ClientLogo/index.js
@@ -5,7 +5,7 @@ import { StyleSheet, Image } from "react-native";
 const styles = StyleSheet.create({ image: { width: 350, height: 100 } });
 
 const ClientLogo = ({ imageSrc }) => (
-  <Image style={styles.image} source={{ src: imageSrc }} />
+  <Image style={styles.image} source={{ uri: imageSrc }} />
 );
 
 ClientLogo.propTypes = { imageSrc: PropTypes.string };
