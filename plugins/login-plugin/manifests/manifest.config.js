2c682f1a115df8e996d030ec6b7f01d1d9707afc
kononenkoAnton <a.kononenko@applicaster.com>
2020-09-08 15:08:23 -0400

    bump iap to 0.3.1-alpha.9

--- plugins/login-plugin/manifests/manifest.config.js
+++ plugins/login-plugin/manifests/manifest.config.js
@@ -2185,7 +2185,7 @@ const extra_dependencies = {
 
 const npm_dependencies = {
   default: [
-    "@applicaster/applicaster-iap@0.3.1-alpha.8",
+    "@applicaster/applicaster-iap@0.3.1-alpha.9",
     "@react-native-community/blur@3.4.1",
   ],
   web: [],
