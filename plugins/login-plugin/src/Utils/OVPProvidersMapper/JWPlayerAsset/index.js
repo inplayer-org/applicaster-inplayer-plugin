da93f25afd9962179eb313e604699479ca64fd4e
kononenkoAnton <a.kononenko@applicaster.com>
2020-09-11 12:26:03 -0400

    Add mapping to content url (#78)

--- plugins/login-plugin/src/Utils/OVPProvidersMapper/JWPlayerAsset/index.js
+++ plugins/login-plugin/src/Utils/OVPProvidersMapper/JWPlayerAsset/index.js
@@ -5,40 +5,9 @@ export const getSrcForJWPlayer = ({ inPlayerContent }) => {
 };
 const JWPlayerContent = (inPlayerContent) => {
   console.log("JWPlayerContent", { inPlayerContent });
-
-  const {
-    mobile_url = null,
-    web_url = null,
-    video_id = null,
-  } = inPlayerContent;
-
-  if (isMobileURLValid({ mobile_url, video_id })) {
-    return mobile_url;
-  }
-  if (isWebURLValid({ web_url, video_id })) {
-    return web_url;
-  }
-
-  return video_id || fallbackURL({ video_id });
-};
-
-const fallbackURL = ({ video_id }) => {
+  const { video_id = null } = inPlayerContent;
+  
   return video_id
     ? `https://content.jwplatform.com/videos/${video_id}.m3u8`
     : null;
-};
-
-const isMobileURLValid = ({ mobile_url, video_id }) => {
-  const mobilePlatform = Platform.OS === "ios" || Platform.OS === "android";
-
-  return mobilePlatform && isUrlValidForPlatform({ video_id, url: mobile_url });
-};
-
-const isWebURLValid = ({ web_url, video_id }) => {
-  const webPlatform = Platform.OS === "web";
-  return webPlatform && isUrlValidForPlatform({ video_id, url: web_url });
-};
-
-const isUrlValidForPlatform = ({ video_id, url }) => {
-  return video_id && video_id.length > 0 && url && url.length > 0;
 };
