3cc2546f340914d48d22110341f48641f69d0350
kononenkoAnton <a.kononenko@applicaster.com>
2020-09-11 12:26:03 -0400

    Add support deafault inplayer assets

--- plugins/login-plugin/src/Utils/OVPProvidersMapper/index.js
+++ plugins/login-plugin/src/Utils/OVPProvidersMapper/index.js
@@ -1,4 +1,3 @@
-import * as R from "ramda";
 import { getInPlayerContent, getInPlayerAssetType } from "../InPlayerResponse";
 import { getSrcForJWPlayer } from "./JWPlayerAsset";
 import { getSrcForHTML } from "./HTMLAsset";
@@ -9,19 +8,61 @@ const SUPPORTED_ASSET_TYPES = {
 };
 
 export const getSrcFromAsset = (inPlayerItemAccess) => {
-  console.log("getSrcFromAsset");
-  if (!inPlayerItemAccess) {
-    return {};
-  }
-  const inPlayerContent = getInPlayerContent(inPlayerItemAccess);
-  const inPlayerAssetType = getInPlayerAssetType(inPlayerItemAccess);
-  console.log("mapOVPProviders", {
-    inPlayerItemAccess,
-    inPlayerContent,
-    inPlayerAssetType,
-  });
-
+  const inPlayerContent =
+    inPlayerItemAccess && getInPlayerContent(inPlayerItemAccess);
   if (inPlayerContent) {
+    const src = retrieveSrcFromDefault({ inPlayerContent });
+
+    if (src) {
+      return src;
+    } else {
+      const fallbackSrc = tryFallBackLogicFromMapping({
+        inPlayerContent,
+        inPlayerItemAccess,
+      });
+      return fallbackSrc;
+    }
+  }
+  return null;
+};
+
+const retrieveSrcFromDefault = ({ inPlayerContent }) => {
+  const { mobile_url = null, web_url = null } = inPlayerContent;
+
+  if (isMobileURLValid({ mobile_url })) {
+    return mobile_url;
+  }
+  if (isWebURLValid({ web_url })) {
+    return web_url;
+  }
+
+  return null;
+};
+
+const isMobileURLValid = ({ mobile_url }) => {
+  const mobilePlatform = Platform.OS === "ios" || Platform.OS === "android";
+
+  return mobilePlatform && isUrlValidForPlatform({ url: mobile_url });
+};
+
+const isWebURLValid = ({ web_url }) => {
+  const webPlatform =
+    Platform.OS === "web" ||
+    Platform.OS === "samsung_tv" ||
+    Platform.OS === "lg_tv";
+  return webPlatform && isUrlValidForPlatform({ url: web_url });
+};
+
+const isUrlValidForPlatform = ({ url }) => {
+  return url && url.length > 0;
+};
+
+const tryFallBackLogicFromMapping = ({
+  inPlayerContent,
+  inPlayerItemAccess,
+}) => {
+  const inPlayerAssetType = getInPlayerAssetType(inPlayerItemAccess);
+  if (inPlayerAssetType) {
     switch (inPlayerAssetType) {
       case SUPPORTED_ASSET_TYPES.JW_ASSET:
         return getSrcForJWPlayer({ inPlayerItemAccess, inPlayerContent });
